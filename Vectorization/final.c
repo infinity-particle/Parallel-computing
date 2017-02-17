b320416748521d9392f5a0a2e1a022102cbaaec3
Sergey Kutsko <serykutsko@gmail.com>
2017-02-17 22:42:53 +0000

    add hand vectorization with AVX

--- Vectorization/final.c
+++ Vectorization/final.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <xmmintrin.h>
+//#include <xmmintrin.h>
 #include <immintrin.h>
 
-#define MATRIX_SIZE 40
-#define ELEMENT_SIZE 10
+#define MATRIX_SIZE 8
+#define ELEMENT_SIZE 4
+
+void printMatrix(double matrix[MATRIX_SIZE][MATRIX_SIZE][ELEMENT_SIZE][ELEMENT_SIZE]);
 
 int main(){
 
@@ -46,37 +48,57 @@ int main(){
 
 	printf("Elapsed time: %f\n", (double)(end - begin)/CLOCKS_PER_SEC);
 
-	/*
 	begin = clock();
 
+	double* temp = (double*)aligned_alloc(16, sizeof(double) * ELEMENT_SIZE);
+	//double temp[ELEMENT_SIZE];
 	for(int row = 0; row < MATRIX_SIZE; row++){
 		for(int column = 0; column < MATRIX_SIZE; column++){
-			__m256 aColumn0 = _mm_setr_pd(A[row][column][0][0], A[row][column][1][0], A[row][column][2][0], A[row][column][3][0]);
-			__m256 aColumn1 = _mm_setr_pd(A[row][column][0][1], A[row][column][1][1], A[row][column][2][1], A[row][column][3][1]);
-			__m256 aColumn2 = _mm_setr_pd(A[row][column][0][2], A[row][column][1][2], A[row][column][2][2], A[row][column][3][2]);
-			__m256 aColumn3 = _mm_setr_pd(A[row][column][0][3], A[row][column][1][3], A[row][column][2][3], A[row][column][3][3]);
+			__m256d aColumn0 = _mm256_setr_pd(A[row][column][0][0], A[row][column][1][0], A[row][column][2][0], A[row][column][3][0]);
+			__m256d aColumn1 = _mm256_setr_pd(A[row][column][0][1], A[row][column][1][1], A[row][column][2][1], A[row][column][3][1]);
+			__m256d aColumn2 = _mm256_setr_pd(A[row][column][0][2], A[row][column][1][2], A[row][column][2][2], A[row][column][3][2]);
+			__m256d aColumn3 = _mm256_setr_pd(A[row][column][0][3], A[row][column][1][3], A[row][column][2][3], A[row][column][3][3]);
 
 			for(int i = 0;  i < ELEMENT_SIZE; i++){
-				__m256 bRow0 = _mm_set1_pd(B[row][column][0][i]);
-				__m256 bRow1 = _mm_set1_pd(B[row][column][1][i]);
-				__m256 bRow2 = _mm_set1_pd(B[row][column][2][i]);
-				__m256 bRow3 = _mm_set1_pd(B[row][column][3][i]);
+				__m256d bRow0 = _mm256_set1_pd(B[row][column][0][i]);
+				__m256d bRow1 = _mm256_set1_pd(B[row][column][1][i]);
+				__m256d bRow2 = _mm256_set1_pd(B[row][column][2][i]);
+				__m256d bRow3 = _mm256_set1_pd(B[row][column][3][i]);
 
-				__m256 cResult = _mm_set1_pd(0.0);
+				__m256d cResult = _mm256_set1_pd(0.0);
 
-				_mm_add_pd(cResult, _mm_mul_pd(aColumn0, bRow0));
-				_mm_add_pd(cResult, _mm_mul_pd(aColumn1, bRow1));
-				_mm_add_pd(cResult, _mm_mul_pd(aColumn2, bRow2));
-				_mm_add_pd(cResult, _mm_mul_pd(aColumn3, bRow3));
+				cResult = _mm256_add_pd(cResult, _mm256_mul_pd(aColumn0, bRow0));
+				cResult = _mm256_add_pd(cResult, _mm256_mul_pd(aColumn1, bRow1));
+				cResult = _mm256_add_pd(cResult, _mm256_mul_pd(aColumn3, bRow3));
+				cResult = _mm256_add_pd(cResult, _mm256_mul_pd(aColumn2, bRow2));
 
+				_mm256_store_pd(temp,cResult);
 
+				C[row][column][0][i] = *(temp + 0);
+				C[row][column][1][i] = *(temp + 1);
+				C[row][column][2][i] = *(temp + 2);
+				C[row][column][3][i] = *(temp + 3);
 			}
 
 			}
-		}
 	}
 
 	end = clock();
-	printf("Elapsed time - Hand vectorization: %f\n", (double)(end - begin)/CLOCKS_PER_SEC);*/
+	printMatrix(C);
+	printf("Elapsed time - Hand vectorization: %f\n", (double)(end - begin)/CLOCKS_PER_SEC);
 	return 0;
+}
+
+void printMatrix(double matrix[MATRIX_SIZE][MATRIX_SIZE][ELEMENT_SIZE][ELEMENT_SIZE]){
+	for(int i = 0; i < MATRIX_SIZE; i++){
+		for(int j = 0; j < MATRIX_SIZE; j++){
+			for(int k = 0; k < ELEMENT_SIZE; k++){
+				for(int n = 0; n < ELEMENT_SIZE; n++){
+					printf("%.3f ", matrix[i][j][k][n]);
+				}
+				printf("\n");
+			}
+			printf("\n\n\n");
+		}
+	}
 }
