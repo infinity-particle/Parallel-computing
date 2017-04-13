b6ce6298361f977602908031662e3735d7ed7a42
Andrew <zakharov-199&@yandex.by>
2017-04-13 10:12:03 +0300

    Code refactored

--- CUDA/Matrix Multiplication/Matrix Multiplication/kernel.cu
+++ CUDA/Matrix Multiplication/Matrix Multiplication/kernel.cu
@@ -14,105 +14,97 @@ using namespace std;
 
 void matrixMultiplicationWithCuda(int A[][MATRIX_SIZE],int B[][MATRIX_SIZE],int C[][MATRIX_SIZE]);
 void matrixMultiplicationCPU(int A[][MATRIX_SIZE],int B[][MATRIX_SIZE],int C[][MATRIX_SIZE]);
+void checkCUDAStatus(cudaError_t cudaStatus);
+bool compareResults(int cudaMultiplicationResult[][MATRIX_SIZE],int cpuMultiplicationResult[][MATRIX_SIZE]);
 
 __global__ void matrixMultiplicationKernel(int *A, int *B, int *C)
 {
 	int result = 0;
-	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	int column = blockIdx.x * blockDim.x + threadIdx.x;
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
 
 
-	if (row > MATRIX_SIZE || col > MATRIX_SIZE) return;
+	if (row > MATRIX_SIZE || column > MATRIX_SIZE) return;
 
 	for (int i = 0; i < MATRIX_SIZE; i++){
-		result += A[row*MATRIX_SIZE + i] * B[i*MATRIX_SIZE + col];
+		result += A[row*MATRIX_SIZE + i] * B[i*MATRIX_SIZE + column];
 	}
-	C[row*MATRIX_SIZE + col] = result;
+	C[row*MATRIX_SIZE + column] = result;
 }
 
 int main()
 {
     srand(time(0));
-	auto A = new int[MATRIX_SIZE][MATRIX_SIZE];
-	auto B = new int[MATRIX_SIZE][MATRIX_SIZE];
-	auto C = new int[MATRIX_SIZE][MATRIX_SIZE];
-	auto cpuMul = new int[MATRIX_SIZE][MATRIX_SIZE];
+	auto matrixA = new int[MATRIX_SIZE][MATRIX_SIZE];
+	auto matrixB = new int[MATRIX_SIZE][MATRIX_SIZE];
+	auto cudaMultiplicationResult = new int[MATRIX_SIZE][MATRIX_SIZE];
+	auto cpuMultiplicationResult = new int[MATRIX_SIZE][MATRIX_SIZE];
 
 	for (int i = 0; i<MATRIX_SIZE; i++){
 		for (int j = 0; j < MATRIX_SIZE; j++){
-			A[i][j] = rand() % 100;
-			B[i][j] = rand() % 100;
-			cpuMul[i][j] = 0;
+			matrixA[i][j] = rand() % 2048;
+			matrixB[i][j] = rand() % 2048;
+			cpuMultiplicationResult[i][j] = 0;
 		}
 	}
 	
 
-	matrixMultiplicationWithCuda(A,B,C);
-	matrixMultiplicationCPU(A,B,cpuMul);
+	matrixMultiplicationWithCuda(matrixA, matrixB, cudaMultiplicationResult);
+	matrixMultiplicationCPU(matrixA, matrixB, cpuMultiplicationResult);
+	if(compareResults(cudaMultiplicationResult, cpuMultiplicationResult)){
+		printf("Results are equals!\n");
+	}else{
+		printf("Results are NOT equals!\n");
+	}
 	system("pause");
-	delete[]A;
-	delete[]B;
-	delete[]C;
+	delete[] matrixA;
+	delete[] matrixB;
+	delete[] cudaMultiplicationResult;
+	delete[] cpuMultiplicationResult;
 }
 
-// Helper function for using CUDA to add vectors in parallel.
 void matrixMultiplicationWithCuda(int A[][MATRIX_SIZE],int B[][MATRIX_SIZE],int C[][MATRIX_SIZE])
 {
     int *dev_a, *dev_b, *dev_c;
-	unsigned long long begin, end;
+	clock_t begin, end;
+	cudaError_t cudaStatus;
 
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-
-	//allocate memory on global memory of gpu
-	cudaError_t err = cudaMalloc((void**)&dev_a, ((MATRIX_SIZE)*(MATRIX_SIZE))*sizeof(int));
-	printf("Cuda malloc A:%s \n", cudaGetErrorString(err));
-	err = cudaMalloc((void**)&dev_b, ((MATRIX_SIZE)*(MATRIX_SIZE))*sizeof(int));
-	printf("Cuda malloc B:%s \n", cudaGetErrorString(err));
-	err = cudaMalloc((void**)&dev_c, ((MATRIX_SIZE)*(MATRIX_SIZE))*sizeof(int));
-	printf("Cuda malloc C:%s \n", cudaGetErrorString(err));
+	cudaStatus = cudaMalloc((void**)&dev_a, ((MATRIX_SIZE)*(MATRIX_SIZE))*sizeof(int));
+	checkCUDAStatus(cudaStatus);
+	cudaStatus = cudaMalloc((void**)&dev_b, ((MATRIX_SIZE)*(MATRIX_SIZE))*sizeof(int));
+	checkCUDAStatus(cudaStatus);
+	cudaStatus = cudaMalloc((void**)&dev_c, ((MATRIX_SIZE)*(MATRIX_SIZE))*sizeof(int));
 
 
-	//Copy array A and B on device allocated memory
-	err = cudaMemcpy(dev_a, A, ((MATRIX_SIZE*MATRIX_SIZE))*sizeof(int), cudaMemcpyHostToDevice);
-	printf("Cuda memcpy to device A:%s \n", cudaGetErrorString(err));
-	err = cudaMemcpy(dev_b, B, ((MATRIX_SIZE*MATRIX_SIZE))*sizeof(int), cudaMemcpyHostToDevice);
-	printf("Cuda memcpy to device B:%s \n", cudaGetErrorString(err));
 
-	//two dimension threads
+	cudaStatus = cudaMemcpy(dev_a, A, ((MATRIX_SIZE*MATRIX_SIZE))*sizeof(int), cudaMemcpyHostToDevice);
+	checkCUDAStatus(cudaStatus);
+	cudaStatus = cudaMemcpy(dev_b, B, ((MATRIX_SIZE*MATRIX_SIZE))*sizeof(int), cudaMemcpyHostToDevice);
+	checkCUDAStatus(cudaStatus);
+
 	dim3 dimBlock(BlockSize, BlockSize);
 	dim3 dimGrid((MATRIX_SIZE + dimBlock.x - 1) / dimBlock.x, (MATRIX_SIZE + dimBlock.y - 1) / dimBlock.y);
 	
-	//call the kernel function multi
-	cudaEventRecord(start);
-	matrixMultiplicationKernel <<< dimGrid, dimBlock >> >(dev_a, dev_b, dev_c);
-	cudaEventRecord(stop);
+	begin = clock();
+	matrixMultiplicationKernel <<< dimGrid, dimBlock >>>(dev_a, dev_b, dev_c);
+	cudaDeviceSynchronize();
+	end = clock();
 
-	//retrieve array C from device memory
-	err = cudaMemcpy(C, dev_c, ((MATRIX_SIZE*MATRIX_SIZE))*sizeof(int), cudaMemcpyDeviceToHost);
-	printf("Cuda memcpy to HOST C:%s \n", cudaGetErrorString(err));
-	cudaEventSynchronize(stop);
-	float milliseconds = 0;
-	cudaEventElapsedTime(&milliseconds, start, stop);
-	printf("CUDA time: %f ms\n", milliseconds);
+	cudaStatus = cudaGetLastError();
+	checkCUDAStatus(cudaStatus);
 
-	/*for (int i = 0; i < MATRIX_SIZE; i++){
-		for (int j = 0; j < MATRIX_SIZE; j++){
-			printf("C(%d,%d) = %d \n", i, j, C[i][j]);
-		}
-	}*/
+	cudaStatus = cudaMemcpy(C, dev_c, ((MATRIX_SIZE*MATRIX_SIZE))*sizeof(int), cudaMemcpyDeviceToHost);
+	checkCUDAStatus(cudaStatus);
+	printf("CUDA time: %lf seconds\n", (double)(end - begin)/CLOCKS_PER_SEC);
 
-	//free the memory
 	cudaFree(dev_a);
 	cudaFree(dev_b);
 	cudaFree(dev_c);
 }
 
 void matrixMultiplicationCPU(int A[][MATRIX_SIZE],int B[][MATRIX_SIZE],int C[][MATRIX_SIZE]){
-	//clock_t begin, end;
-
-	clock_t begin = clock();
+	clock_t begin, end;
+	begin = clock();
 	for (int row = 0; row < MATRIX_SIZE; row++) {  
 		for (int col = 0; col < MATRIX_SIZE; col++) {  
 			for (int inner = 0; inner < MATRIX_SIZE; inner++) {  
@@ -120,6 +112,24 @@ void matrixMultiplicationCPU(int A[][MATRIX_SIZE],int B[][MATRIX_SIZE],int C[][M
 			}   
 		}  
 	} 
-	clock_t end = clock();
-	printf("CPU time: %llf sec\n", (double)(end - begin)/CLOCKS_PER_SEC);
+	end = clock();
+	printf("CPU time: %lf seconds\n", (double)(end - begin)/CLOCKS_PER_SEC);
+}
+
+void checkCUDAStatus(cudaError_t cudaStatus){
+	if(cudaStatus != cudaSuccess){
+		printf("CUDA return error code: %d", cudaStatus);
+		exit(-1);
+	}
+}
+
+bool compareResults(int cudaMultiplicationResult[][MATRIX_SIZE],int cpuMultiplicationResult[][MATRIX_SIZE]){
+	for(int row = 0; row < MATRIX_SIZE; row++){
+		for(int column = 0; column < MATRIX_SIZE; column++){
+			if(cudaMultiplicationResult[row][column] != cpuMultiplicationResult[row][column]){
+				return false;
+			}
+		}
+	}
+	return true;
 }
